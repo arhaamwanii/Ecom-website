123eeeeb79409605b6b109222f9f8c7ae1a2aafe
arhaamwanii <arhamwani765@gmail.com>
2024-01-03 22:37:01 +0530

    .

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Navbar from './Components/Navbar/Navbar';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <Navbar/>
   <h1> Trial code</h1>
     </>
   )
