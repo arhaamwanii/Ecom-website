0378d2f3a5a20de85f6f8695edf483d4307a5a1b
arhaamwanii <arhamwani765@gmail.com>
2024-01-04 16:39:48 +0530

    .

--- Frontend/src/Components/Navbar/Navbar.jsx
+++ Frontend/src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import './Navbar.css'
+import logo from'../assets/logo.png'
+import cart_icon from '../assets/cart_icon.png'
 
 function Navbar() {
   return (
     <div className='navbar'>
-        
+    <logo/>
+    <h1> nav bar trial</h1>
     </div>
   )
 }
