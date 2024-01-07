7f05b2457f2c710798c1f083b303ad38a18a8dbd
arhaamwanii <arhamwani765@gmail.com>
2024-01-07 22:22:50 +0530

    .

--- tic-tak/src/App.jsx
+++ tic-tak/src/App.jsx
@@ -36,3 +36,4 @@ export default App
 
 //what should i build or should i learn
 //the thing is I havent finished the cource whivh i bought like a 4 months ago
+//i can go ahead and finish this cource which is going to take forver or i can not do that and go straight into biulding with no prior knowlege
