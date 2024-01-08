eea88a432834de238ad3fdc96116e877048c61c2
arhaamwanii <arhamwani765@gmail.com>
2024-01-08 22:30:57 +0530

    .

--- tic-tak/src/App.jsx
+++ tic-tak/src/App.jsx
@@ -40,9 +40,20 @@ export default App
 //the thing is inorder to apply for a start where i am going to offer them, that i am going to work for free for you it wont take that much learning it is just isnt going to take much i will learn there and in that environment i wil be forced to do shit also here to a great level it a also an personal attempt to run away form failure because it is true that this is going to be hard -- i have this inate tendecy to optimize to level where it becomes over optimization there is some truth to it that this ia  great thing in some cases but when you trying to so things fast this is not the way to about it and right now in my case particularly i need to do shit fast 
 //can i apply to startup right now in this moment i most probably can i just offer them to work for them for free then we will see how it goes on from there, there is a lekly chance that they will accept the offer and allow me to wor
 //the cource if only the main main thigs are done can be done realistically in two weeks
+//arham ami wanit is acj  
+//at this point i dont even know what i am doing it is like i am competing with 10 people in 10 different ways will end up winning nothing
 
 //    !!!  fuck everytihing and finish the cource finish the cource and appply to a buch of startups and get to work
 //yeah anoter comment
+
+
+//I am competing with 3 niches in where all three all they are doing is there thing at that poitn
+// 1. stream/youtube >> and these two are basically the same
+// 2. studying       >>
+// 3 learning to code  > these two are basically the same 
+// 3. making a startup > 
+
+//
 
 
 
